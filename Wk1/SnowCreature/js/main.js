1df5522bcb1ffcacf2a0f0367fef20de0e1ed28b
brianrog9 <103602092+brianrog9@users.noreply.github.com>
2023-01-22 16:35:33 -0600

    Attemp1

--- Wk1/SnowCreature/js/main.js
+++ Wk1/SnowCreature/js/main.js
@@ -1,35 +1,51 @@
 let grid = undefined;
 let stroke1 = prompt("enter a basic color name in lower case", "blue");
 let stroke2 = prompt("enter another basic color name in lower case", "yellow");
+let bgcolor = ("#E6F8FF");
+let fillcolor = ("#CAF7F0");
+let x, y;
+x = 1000 / 2;
+y = 800;
+
 function setup(){
     createCanvas(1000, 800);
-    background("#ccc");
+    background(bgcolor);
 }
 
 function draw() {
-    fill ("#f1f1f1");
+    fill (fillcolor);
     stroke(stroke1);
     strokeWeight(20);
-    //left
-    ellipse (350, 650, 200);
-    //right
-    ellipse(650, 650, 200);
+    //body2
+    ellipse(500, 600, 360);
     //body
-    ellipse(500, 450, 350, 400);
+    ellipse(500, 400, 270);
     //head
     ellipse(500, 200, 200);
     // hat
     stroke(stroke2);
-    strokeWeight(40);
+    strokeWeight(20);
     line(400, 120, 600, 120);
+    fill(stroke2);
     quad(400, 50, 600, 50, 550, 120, 450, 120);
-    //eyes
+    //eyes pupils
     stroke(0);
-    strokeWeight(50);
-    point (425, 200);
-    point (575, 200);
+    strokeWeight(30);
+    point (465, 180);
+    point (535, 180);
     //mouth
     noFill();
     strokeWeight(10);
-    arc (500, 200, 80, 80, 0, HALF_PI);
+    arc (480, 220, 60, 60, 0, HALF_PI);
+    //snow
+    strokeWeight(5);
+    stroke("#E6F8FF");
+    fill("#FFFFFF");
+    ellipse(x, y, 30, 30);
+
+    x = x + random (-1, 1);
+    y = y + 1;
+    if (y > 810) {
+        y = 1;
+    }
 }
