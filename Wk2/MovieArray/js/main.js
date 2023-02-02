a9147e698a62cd77602fc4b14651d9d578c7fec5
brianrog9 <103602092+brianrog9@users.noreply.github.com>
2023-02-02 14:08:44 -0600

    Update main.js

---
+++ Wk2/MovieArray/js/main.js
@@ -0,0 +1,6 @@
+let favoriteMovies =[' The Lion King ','Star Wars Rouge One ','Lord of the Rings ','Harry Potter ','The Incredibles '];
+console.log(favoriteMovies);
+let movie1 = prompt("What is your favorite movie?");
+favoriteMovies.push(movie1);
+console.log(favoriteMovies);
+alert("Our favorite movies are" + favoriteMovies.toString());
