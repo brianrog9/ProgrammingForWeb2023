80873e9f728652d830be78e410617ab9cd4da2c3
brianrog9 <103602092+brianrog9@users.noreply.github.com>
2023-02-05 17:26:55 -0600

    Update main.js

--- Wk2/ArrayOfQuestions/js/main.js
+++ Wk2/ArrayOfQuestions/js/main.js
@@ -1,9 +1,24 @@
 //Array of Questions
 let quiz = [
-    {question: "what?", answer:"this"},
-    {question: "what?", answer:"this"},
-    {question: "what?", answer:"this"},
+    {question: "What's the capital of Montana?", answer:"Helena"},
+    {question: "How many original Pokemon are there?", answer:"151"},
+    {question: "How wide is a football field?", answer:"53.3 yards wide"},
 ];
 
 //check question array
-console.log(quiz);
+console.log ("quiz questions", quiz);
+
+//random question prompt
+const quizindex = Math.floor(Math.random() * quiz.length);
+const answer = window.prompt(quiz[quizindex].question);
+
+//user answer check
+console.log("user answered", answer);
+
+//feedback
+window.alert("You answered " + answer + " The correct answer is: " + quiz[quizindex].answer);
+
+
+
+
+
