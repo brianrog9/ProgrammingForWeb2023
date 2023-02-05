3d57134409a7d66d15be2dd43fc1f3b190119acf
brianrog9 <103602092+brianrog9@users.noreply.github.com>
2023-02-05 17:04:08 -0600

    Update main.js

---
+++ Wk2/ArrayOfQuestions/js/main.js
@@ -0,0 +1,9 @@
+//Array of Questions
+let quiz = [
+    {question: "what?", answer:"this"},
+    {question: "what?", answer:"this"},
+    {question: "what?", answer:"this"},
+];
+
+//check question array
+console.log(quiz);
