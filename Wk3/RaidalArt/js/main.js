40e799b7b196939af82040654ef5bc0d782e23e9
brianrog9 <103602092+brianrog9@users.noreply.github.com>
2023-02-05 21:30:54 -0600

    Update main.js

--- Wk3/RaidalArt/js/main.js
+++ Wk3/RaidalArt/js/main.js
@@ -1,50 +1,81 @@
+
+// Color arrays
+let circleone = ["#ffe11c","#ffffff", '#33f56d'];
+let circletwo = ["#f53333", '#eb33f5', '#9b33f5'];
+let circlethree = ["#33e8f5", '#f57a33', '#77f533'];
+
+//random color selection
+let coloroneindex = Math.floor(Math.random() * circleone.length);
+let coloroneset = circleone[coloroneindex];
+let colortwoindex = Math.floor(Math.random() * circletwo.length);
+let colortwoset = circletwo[colortwoindex];
+let colorthreeindex = Math.floor(Math.random() * circlethree.length);
+let colorthreeset = circlethree[colorthreeindex];
+
 let rotateBy = 5;
-let circleone = ("#ffe11c");
-let cirlcetwo = ("#f53333");
-let circlethree= ("#33e8f5");
+
+//button locations
 let loopcheck = true;
 let rectW = 100;
 let rectH = 100;
 let x = 608;
 let y = 408;
 let sz = 100;
+// let x1 = 608;
+// let y1 = 508;
+// let sz1 = 100;
+let x2 = 608;
+let y2 = 308;
+let sz2 = 100;
+
+
 
 function setup() {
     createCanvas(600, 600);
     background(0);
     angleMode(DEGREES);
-    changeButton = createButton('Change');
-    changeButton.size(100,100);
-    changeButton.position(608, 508);
+    resetButton = createButton('Change')
+    resetButton.size(100, 100);
+    resetButton.position(608, 308);
+    // changeButton = createButton('Change');
+    // changeButton.size(100,100);
+    // changeButton.position(608, 508);
     stopButton = createButton('Pause');
     stopButton.size(100,100);
     stopButton.position(608, 408);
     
 }
 
+
+//Elipse one
 function makeArm(rotateBy) {
     let alt = Math.round(rotateBy / 360);
     noFill();
-    stroke(circleone);
+    stroke(coloroneset);
     strokeWeight(1);
     ellipse(150, 150, + alt, 150 - alt);
 }
 
+
+//Elipse two
 function makeArmTwo(rotateBy) {
-    let alt = Math.round(rotateBy / 360);
+    let alt = Math.round(rotateBy / 60);
     noFill();
-    stroke(cirlcetwo);
+    stroke(colortwoset);
     strokeWeight(1);
-    ellipse(100, 100, + alt, 100 - alt);
+    ellipse(100, 100, + alt, 100 / alt);
 }
 
+
+//Elipse three
 function makeArmThree(rotateBy) {
-    let alt = Math.round(rotateBy / 360);
+    let alt = Math.round(rotateBy / 370);
     noFill();
-    stroke(circlethree);
+    stroke(colorthreeset);
     strokeWeight(1);
-    ellipse(175, 175 + alt, 175 - alt);
+    ellipse(175, 175, + alt, 175 + alt);
 }
+
 
 function draw() {
     translate (300, 300);
@@ -55,6 +86,8 @@ function draw() {
     rotateBy += 5;
 }
 
+
+//Reset and Pause Functions
 function mousePressed() {
     if (mouseX > x && mouseY > y && mouseX < x + sz && mouseY < y + sz){
         loopcheck = !loopcheck;
@@ -67,8 +100,22 @@ function mousePressed() {
     else if (!loopcheck){
         noLoop();
     }
+    // if (mouseX > x1 && mouseY > y1 && mouseX < x1 + sz1 && mouseY < y1 + sz1){
+    //     console.log('pressed');
+        
+    // }
+    if (mouseX > x2 && mouseY > y2 && mouseX < x2 + sz2 && mouseY < y2 + sz2){
+        console.log('pressed');
+        restart();
+    }
 
 }
+
+function restart(){
+    window.location.reload();
+}
+
+
 
 
 
