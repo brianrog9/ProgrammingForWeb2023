4ab9c377f35e371a6607cc86a0f5a5131abd3604
brianrog9 <103602092+brianrog9@users.noreply.github.com>
2023-02-16 11:45:30 -0600

    Update main.js

---
+++ wk5/Ex5.1/js/main.js
@@ -0,0 +1,44 @@
+let blockX = 0;
+let blockY = 0;
+let drawTimer;
+let speed = 15;
+let distance = 2;
+let blockColor = 255;
+
+function setup () {
+    createCanvas(500, 500);
+    background(0);
+}
+
+function drawBlock(x, y, color) {
+    fill(color || 255);
+    rect(x, y, 50, 50);
+}
+
+function keyTyped() {
+     let keyToNumber = Number(key);
+     if (isNaN(keyToNumber)) {
+        return;
+     }
+     keyToNumber = map(keyToNumber, 1, 9, 1, 255);
+     console.log('converted number', keyToNumber);
+     blockColor = keyToNumber;
+}
+
+window.setTimeout(() => {
+    drawTimer = window.setInterval(() => {
+        if (blockY - 50 <= height) {
+            drawBlock(blockX, blockY, blockColor);
+            blockY += distance;
+        } else {
+            blockY = 0;
+            blockX += 50;
+        }
+        if (blockY - 50 > height && blockX - 50 > width){
+            window.clearInterval(drawTimer);
+            alert('done!');
+        }
+       
+    }, speed);
+}, 1500);
+
