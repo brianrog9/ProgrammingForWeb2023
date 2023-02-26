940671353d003a79c59c4518fcbbc126fbb24650
brianrog9 <103602092+brianrog9@users.noreply.github.com>
2023-02-26 12:01:56 -0600

    CSS added

---
+++ Wk6/VueLlama/js/main.js
@@ -0,0 +1,19 @@
+const {createApp} = Vue;
+
+createApp({
+    data() {
+        return {
+            Lbite: 'Llamas dont bite',
+            Lveg: 'Llamas are vegetarian',
+            Lage: 'Llamas live to be about 20 years old',
+            Lbaby: 'Baby llamas are called a cira',
+            Lpop: 'There are about 7million llamas in South America',
+            Lsize: 'Llamas are twice the size of alpacas',
+            Lguard: 'Llamas are natural guards requiring almost no training',
+            Lcarry: 'Llamas can carry 200 pounds for 12 hours a day',
+            Lspit: 'A llama can spit up to 15 feet or more',
+            Langry: 'When one llama is angry at another llama, they will stick their tongue out at them', 
+            curious: true        
+        }
+    }
+}).mount('#myApp');
