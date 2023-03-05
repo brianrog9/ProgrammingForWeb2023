8562316011e56b039a376214d743f617b60bc17d
brianrog9 <103602092+brianrog9@users.noreply.github.com>
2023-03-05 12:42:23 -0600

    framework setup

---
+++ Wk7/ShowandTell/js/main.js
@@ -0,0 +1,41 @@
+let vm = new Vue({
+    el: "#myCollectionApp",
+    data: {
+        boardgames: [{
+            gameName:
+            image:
+            releaseDate:
+            players:
+            description:
+        },
+        {
+            gameName:
+            image:
+            releaseDate:
+            players:
+            description:
+        },
+        {
+            gameName:
+            image:
+            releaseDate:
+            players:
+            description:
+        },
+        {
+            gameName:
+            image:
+            releaseDate:
+            players:
+            description:
+        },
+        {
+            gameName:
+            image:
+            releaseDate:
+            players:
+            description:
+        },
+       ]
+    }
+}.mount("#myCollectionApp"));
